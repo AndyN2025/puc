7d05fe27210129e93654771e63918b9ec70c27ba
AndyN2025 <AndyN2025@users.noreply.github.com>
2026-05-08 19:49:15 +0000

    deploy: af816b7dd44c00b9cadea2ee4ed2d3a5b63311e7

---
+++ _nuxt/CTpiX515.js
@@ -0,0 +1,1 @@
+import"./D1Pi2wvU.js";const r=""+new URL("Лицензия на осуществление образовательной деятельности.C2QuS9Fs.pdf",import.meta.url).href;export{r as L};
--- _nuxt/mAAf-H_w.js
+++
@@ -1,1 +0,0 @@
-import"./Y6kt2aRQ.js";const r=""+new URL("Лицензия на осуществление образовательной деятельности.C2QuS9Fs.pdf",import.meta.url).href;export{r as L};
